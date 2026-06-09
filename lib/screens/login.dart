bee87ee97e7a662d429505768ad4c63493b19561
Maksim Hrusheuski <m.grushevsky10@gmail.com>
2026-06-09 14:32:27 +0200

    Added logo and title

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -12,7 +12,22 @@ class LoginScreen extends StatelessWidget {
           children: [
             SingleChildScrollView(
               padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Column(children: [const SizedBox(height: 48)]),
+              child: Column(children: [
+                const SizedBox(height: 48),
+                Image.asset('assets/imaages/nike.png', height: 48,),
+                const SizedBox(height: 24),
+                const Text(
+                    'YOUR ACCOUNT FOR\nEVERYTHING NIKE',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.5,
+                        height: 1.4,
+                    )
+                ),
+                const SizedBox(height: 32),
+              ]),
             ),
 
             Positioned(
